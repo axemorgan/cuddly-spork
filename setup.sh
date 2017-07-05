eceb19eaf9ab8383bb58aa8dcf16a9eaaee3a50a
MORGAN, ALEX <alex.morgan@kroger.com>
2017-07-05 13:06:27 -0400

    Added a prompt to the script to warn about overwriting current dotfiles

--- setup.sh
+++ setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+#Exit Codes
 EXIT_DOTFILES_DIR_DOESNT_EXIST=10
 
 
+# Warn user about overwriting current dotfiles
+while true; do
+	read -p "Warning: this script will overwrite your current dotfiles. Continue? [y/n] " response
+	case $response in
+		[Yy]* ) break;;
+		[Nn]* ) exit;;
+		* ) echo "Please answer yes or no";;
+	esac
+done
+
 echo Beginning dotfiles setup
 
-##
-# Symlink the files in shell
-##
 DOTFILES_DIR="$HOME/dotfiles"
 
 cd $DOTFILES_DIR
