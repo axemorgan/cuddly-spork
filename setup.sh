4ee1fe00f480452f3a2f186f1909aa909e1831ac
Alex Morgan <axemorgan@gmail.com>
2017-07-06 16:42:42 -0400

    Check for and install homebrew during setup

--- setup.sh
+++ setup.sh
@@ -1,51 +1,53 @@
 #!/bin/bash
 
+EXIT_CODE_HOMEBREW_INSTALL_FAILED=99
+
 ##
 # Runs the given command and suppresses its output
 ##
 silently() {
-source "$1" > /dev/null 2>&1
+	eval $1 > /dev/null 2>&1
 }
 
 ##
 # Warn user about overwriting current dotfiles
 ##
 overwrite_warning() {
-while true; do
-	read -p "Warning: this script will overwrite any current dotfiles. Continue? [y/n] " response
-	case $response in
-		[Yy]* ) break;;
-		[Nn]* ) exit;;
-		* ) printf "Please answer yes or no\n";;
-	esac
-done
+	while true; do
+		read -p "Warning: this script will overwrite any current dotfiles. Continue? [y/n] " response
+		case $response in
+			[Yy]* ) break;;
+			[Nn]* ) printf "Setup cancelled\n"; exit;;
+			* ) printf "Please answer yes or no\n";;
+		esac
+	done
 
-printf "\n"
+	printf "\n"
 }
 
 
 ##
 # Create the dotfiles directory if necessary and backup existing files
 ##
-create_and_backup() {
-SCRIPT_DIR=$(pwd)
-DOTFILES_DIR="$HOME/dotfiles"
-BACKUP_DIR="$HOME/dotfiles_old"
+backup_and_create_dir() {
+	SCRIPT_DIR=$(pwd)
+	DOTFILES_DIR="$HOME/dotfiles"
+	BACKUP_DIR="$HOME/dotfiles_old"
 
-# Check for an existing dotfiles directory
-if [[ -d $DOTFILES_DIR ]]; then	
-	printf "Found existing dotfiles. Backing up to $BACKUP_DIR\n"
-	mkdir -p "$BACKUP_DIR"
-	cp -R "$DOTFILES_DIR"/ "$BACKUP_DIR"
-	rm -r "$DOTFILES_DIR"
-	mkdir -p "$DOTFILES_DIR"
-	printf "Backup finished\n"
-else
-	mkdir -p $DOTFILES_DIR
-	printf "Created dotfiles directory\n"
-fi
+	# Check for an existing dotfiles directory
+	if [[ -d $DOTFILES_DIR ]]; then	
+		printf "Found existing dotfiles. Backing up to $BACKUP_DIR\n"
+		mkdir -p "$BACKUP_DIR"
+		cp -R "$DOTFILES_DIR"/ "$BACKUP_DIR"
+		rm -r "$DOTFILES_DIR"
+		mkdir -p "$DOTFILES_DIR"
+		printf "Backup finished\n"
+	else
+		mkdir -p $DOTFILES_DIR
+		printf "Created dotfiles directory\n"
+	fi
 
-printf "\n"
+	printf "\n"
 }
 
 
@@ -53,34 +55,39 @@ printf "\n"
 # Shell Configuration
 ##
 configure_shell() {
-printf "Copying shell scripts...\n"
-cp -R "$SCRIPT_DIR/shell" "$DOTFILES_DIR"
+	printf "Copying shell scripts...\n"
+	cp -R "$SCRIPT_DIR/shell" "$DOTFILES_DIR"
 
-printf "Setting up bash_profile...\n"
-ln -fs $DOTFILES_DIR/shell/bash_profile "$HOME/.bash_profile"
+	printf "Setting up bash_profile...\n"
+	ln -fs $DOTFILES_DIR/shell/bash_profile "$HOME/.bash_profile"
 
-printf "Shell configuration complete\n"
-printf "\n"
+	printf "Shell configuration complete\n"
+	printf "\n"
 }
 
+
+install_homebrew() {
+	printf "Installing homebrew...\n"
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
 
 ##
 # Install Sublime Text
 ##
-install_sublime() {
-printf "Downloading Sublime text package...\n"
+# install_sublime() {
+# printf "Downloading Sublime text package...\n"
 
-SUBLIME_PACKAGE="https://download.sublimetext.com/Sublime%20Text%20Build%203126.dmg"
-SUBLIME_DOWNLOAD="$HOME/Downloads/Sublime Text.dmg"
+# SUBLIME_PACKAGE="https://download.sublimetext.com/Sublime%20Text%20Build%203126.dmg"
+# SUBLIME_DOWNLOAD="$HOME/Downloads/Sublime Text.dmg"
 
-curl "$SUBLIME_PACKAGE" --output "$SUBLIME_DOWNLOAD"
+# curl "$SUBLIME_PACKAGE" --output "$SUBLIME_DOWNLOAD"
 
-hdiutil attach "$SUBLIME_DOWNLOAD"
+# hdiutil attach "$SUBLIME_DOWNLOAD"
 
-cp -R "/Volumes/Sublime Text" "/Applications/Sublime Text"
-printf "Sublime Text successfully installed\n"
-printf "\n"
-}
+# cp -R "/Volumes/Sublime Text" "/Applications/Sublime Text"
+# printf "Sublime Text successfully installed\n"
+# printf "\n"
+# }
 
 
 #Install homebrew if not present yet
@@ -101,9 +108,22 @@ printf "\n"
 ##
 overwrite_warning
 
-create_and_backup
+backup_and_create_dir
 
 configure_shell
 
+silently "brew -v"
+if [[ $? -eq 0 ]]; then
+	printf "Homebrew is already installed\n"
+else
+	install_homebrew
+	if [[ $? -eq 0 ]]; then
+		printf "Homebrew successfully installed\n"
+	else
+		printf "Failed to install homebrew\n"
+		exit $EXIT_CODE_HOMEBREW_INSTALL_FAILED
+	fi
+fi
+printf "\n"
 
 echo Setup complete!
