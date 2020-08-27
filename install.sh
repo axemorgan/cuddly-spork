534cad8569c194feb011a382454ec36661692b88
Alex Morgan <alex.morgan@kroger.com>
2020-08-27 10:25:36 -0400

    Move all the configuration blocks into functions

--- install.sh
+++ install.sh
@@ -30,47 +30,62 @@ function clone_or_update_repo() {
     fi
 }
 
+# Setup proxy variables for Spike if needed
+function setup_shell_proxy() {
+    read -p "Is this machine proxied by Spike? (y/n): " PROXIED
+    if [ $PROXIED == 'y' ]; then
+        export http_proxy="http://127.0.0.1:3128"
+        export https_proxy=$http_proxy
+        export ftp_proxy=$http_proxy
+        export rsync_proxy=$http_proxy
+        export HTTP_PROXY=$http_proxy
+        export HTTPS_PROXY=$http_proxy
+        export FTP_PROXY=$http_proxy
+        export RSYNC_PROXY=$http_proxy
+    fi
+}
+
+# Downloads and installs oh my zsh and then configures it
+function install_zsh() {
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    # Symlink to the zsh custom plugin
+    ln -sFf "$REPO_DIR/configuration.zsh" "$ZSH/custom/configuration.zsh"
+
+    # Restart zsh to apply the changes to the current session
+    exec zsh
+}
+
+# Configures some default git settings globally
+function configure_git() {
+    git config --global user.name "Alex Morgan"
+    git config --global user.email "axemorgan@gmail.com"
+    git config --global core.pager "more"
+}
+
+# Checks Github for an SSH key for this machine, creating and uploading one if needed
+function setup_github_ssh() {
+    echo "Connecting Github SSH..."
+    read -p "Give a name to identify this machine: " MACHINE_NAME
+    EXISTING_SSH_KEY=$(curl -sS -u axemorgan -H "Accept: application/vnd.github.v3+json" https://api.github.com/user/keys | jq ".[] | select(.title == \"$MACHINE_NAME\")")
+
+    if [ ! -z "$EXISTING_SSH_KEY" ]; then
+        echo "Found an existing SSH key on Github"
+        jq . <<<"$EXISTING_SSH_KEY"
+    else
+        echo "Generating an SSH key and uploading to Github"
+        ssh-keygen -q -t RSA -f "$HOME/.ssh/${MACHINE_NAME// /_}" -N ""
+        PUBLIC_KEY=$(cat $HOME/.ssh/${MACHINE_NAME// /_}.pub)
+        curl -sS -u axemorgan -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/user/keys -d "{\"title\":\"$MACHINE_NAME\", \"key\":\"$PUBLIC_KEY\"}" >/dev/null
+        echo "$HOME/.ssh/${MACHINE_NAME// /_}.pub was added to Github"
+    fi
+}
+
 echo "Installing dotfiles..."
-
-# Setup proxy variables for Spike
-read -p "Is this machine proxied by Spike? (y/n): " PROXIED
-if [ $PROXIED == 'y' ]; then
-    export http_proxy="http://127.0.0.1:3128"
-    export https_proxy=$http_proxy
-    export ftp_proxy=$http_proxy
-    export rsync_proxy=$http_proxy
-    export HTTP_PROXY=$http_proxy
-    export HTTPS_PROXY=$http_proxy
-    export FTP_PROXY=$http_proxy
-    export RSYNC_PROXY=$http_proxy
-fi
-
+setup_shell_proxy
 create_projects_directory
 clone_or_update_repo
-
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Symlink to the zsh custom plugin then refresh to load the new config
-ln -sFf "$REPO_DIR/configuration.zsh" "$ZSH/custom/configuration.zsh"
-exec zsh
-
-# Configure Git
-git config --global user.name "Alex Morgan"
-git config --global user.email "axemorgan@gmail.com"
-git config --global core.pager "more"
-
-echo "Connecting Github SSH..."
-read -p "Give a name to identify this machine: " MACHINE_NAME
-EXISTING_SSH_KEY=$(curl -sS -u axemorgan -H "Accept: application/vnd.github.v3+json" https://api.github.com/user/keys | jq ".[] | select(.title == \"$MACHINE_NAME\")")
-
-if [ ! -z "$EXISTING_SSH_KEY" ]; then
-    echo "Found an existing SSH key on Github"
-    jq . <<<"$EXISTING_SSH_KEY"
-else
-    echo "Generating an SSH key and uploading to Github"
-    ssh-keygen -q -t RSA -f "$HOME/.ssh/${MACHINE_NAME// /_}" -N ""
-    PUBLIC_KEY=$(cat $HOME/.ssh/${MACHINE_NAME// /_}.pub)
-    curl -sS -u axemorgan -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/user/keys -d "{\"title\":\"$MACHINE_NAME\", \"key\":\"$PUBLIC_KEY\"}" >/dev/null
-    echo "$HOME/.ssh/${MACHINE_NAME// /_}.pub was added to Github"
-fi
+install_zsh
+configure_git
+setup_github_ssh
+echo "Done!"
